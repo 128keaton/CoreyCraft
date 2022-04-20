3bde3a0f43b0b46ae6cfb24a50d7fd4feaad344d
Keaton Burleson <keaton.burleson@me.com>
2022-04-19 20:47:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,7 @@ $.getJSON(url, function(r) {
     return false;
   }
   
-  var pl = '';
-  if (r.players.sample.length > 0) {
-    pl = '<br>OP: ' + r.players.sample[0].name;
-  }
   
-  $('#rest').html(r.description.replace(/§(.+?)/gi, '') + '<br><b>Players Online:</b> ' + r.players.online + pl);
+  $('#rest').html(r.description.replace(/§(.+?)/gi, '') + '<br><b>Players Online:</b> ' + r.players.online);
   $('#favicon').attr('src', r.favicon);
 });
